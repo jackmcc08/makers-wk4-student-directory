564bc909f8e470675e74dcf90f35988931b01ac3
Jack McCarthy <jackmcc08@gmail.com>
2021-02-15 10:55:41 +0000

    reconfigured code to accept user input

--- directory.rb
+++ directory.rb
@@ -1,5 +1,6 @@
 # let's put all students into an array
-
+# commenting out existing students array to replace with user input array
+=begin
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -13,6 +14,26 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november}, 
   {name: "Norman Bates", cohort:  :november}
 ]
+=end
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # empty array
+  students = []
+  # get name
+  name = gets.chomp
+  # while name is not empty, repeat the input process
+  while !name.empty? do
+    #add the hash to array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
 
 def print_header
   puts "The students of Villains Acadmey"
@@ -28,6 +49,7 @@ def print_footer(names)
 end
 
 # Method calls
+students = input_students
 print_header
 print(students)
 print_footer(students)
