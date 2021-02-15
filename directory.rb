90a5e34e78674a86c8af79544f63beaf4817c6f2
Jack McCarthy <jackmcc08@gmail.com>
2021-02-15 10:41:20 +0000

    Put names into array with cohort month

--- directory.rb
+++ directory.rb
@@ -1,24 +1,34 @@
 # let's put all students into an array
 
 students = [
-"Dr. Hannibal Lecter",
-"Darth Vader",
-"Nurse Ratched",
-"Michael Corleone",
-"Alec DeLarge",
-"The Wicked Witch of the West",
-"Terminator",
-"Freddy Krueger",
-"The Joker",
-"Joffrey Baratheon", 
-"Norman Bates"
+  ["Dr. Hannibal Lecter",:november],
+  ["Darth Vader",:november],
+  ["Nurse Ratched",:november],
+  ["Michael Corleone",:november],
+  ["Alec DeLarge",:november],
+  ["The Wicked Witch of the West",:november],
+  ["Terminator",:november],
+  ["Freddy Krueger",:november],
+  ["The Joker",:november],
+  ["Joffrey Baratheon",:november], 
+  ["Norman Bates", :november]
 ]
 
-# and then print them
-puts "The students of Villains Acadmey"
-puts "______________"
+def print_header
+  puts "The students of Villains Acadmey"
+  puts "______________"
+end
 
-students.each { |x| puts x}
+def print(names)
+ names.each { |name| puts "#{name[0]} (#{name[1]} cohort)" }
+end
 
-#finally, we print the total number of students
-puts "Overall, we have #{students.count} great students"
+def print_footer(names)
+	puts "Overall, we have #{names.count} great students"
+end
+
+# Method calls
+print_header
+print(students)
+print_footer(students)
+
