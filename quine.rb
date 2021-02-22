a5d6c6d65824f54dd4a5ea8421e35e7fdc9dbae1
Jack McCarthy <jackmcc08@gmail.com>
2021-02-22 14:17:00 +0000

    FINISHED - student Directory - more refactoring possible, but putting to side for now. I have also written several solutions for quine program

--- quine.rb
+++ quine.rb
@@ -1,14 +1,23 @@
 # Quine Program - a program that prints out its own source code
 # (self-replicating program....?)
-File.open(__FILE__,"r").readlines.each { |line| print "#{line}"} 
-str = <<-str   
+File.open(__FILE__,"r").readlines.each { |line| print "#{line}"}
+
+#proper quine below
+puts "------\n\n"
+
+puts IO.readlines(__FILE__)
+puts File.read(__FILE__)
+
+str = <<-str
 puts "str = <<-str"
 puts str
 puts "str"
 puts str
 str
-puts "str = <<-str"
-puts str
-puts "str"
-puts str
+# puts "str = <<-str"
+# puts str
+# puts "str"
+# puts str
 
+#File.read
+#Rspec - point to places or error
---
+++ quine_final.rb
@@ -0,0 +1,1 @@
+File.open(__FILE__,"r").readlines.each { |line| print "#{line}"}
